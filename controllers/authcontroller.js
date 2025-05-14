4e95033b0efeb441f3377d52307a40006cfcd0e5
haseebt456 <m.haseebtufail@gmail.com>
2025-05-14 22:55:52 +0500

    change error message

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -56,7 +56,7 @@ exports.login = async (req, res) => {
 
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(401).json({ error: 'Invalid credentials' });
+    if (!isMatch) return res.status(401).json({ error: 'Invalid Password' });
 
 
     if (user.role !== role) {
